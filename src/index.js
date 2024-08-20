2ab7a67a28b7d2295b7b0178b0707afb31f5605a
Lucas <lucastassi@live.com>
2024-08-20 17:11:28 -0300

    Atualiza modal

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ export default Feedback
 const detect = () => {
     console.log("FEEDBACK: Detect acionado");
     window.feedback = new Feedback({'emailField': false, 'forceShowButton': true})
-    console.log("FEEDBACK: Feedback criado");
-
-    /*
+    
     // Render the default button
     if (window.feedback.options.forceShowButton) {
         console.log("FEEDBACK: Render button delegado");
         window.feedback.renderButton()
     }
-    */
 }
 
 console.log("FEEDBACK: Entrada no script");
